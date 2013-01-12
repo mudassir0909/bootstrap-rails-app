f9776ad8636db15abdb7c5b0f162e7259f4825ba
mmuddasir <mudassir.dev@gmail.com>
2013-01-12 17:29:32 +0530

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    base_title = "Page"
+    if @title.nil?
+      "Rails Application flavored with Bootstrap :) "
+    else
+      "#{@title} #{base_title}"
+    end
+  end
 end
